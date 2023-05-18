edbb7b1e347d9ed9cad27036b81d9ff7e9857d17
cleclair71 <cassieleclair71@gmail.com>
2023-05-18 17:23:09 -0400

    auth tweaking

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,17 +6,17 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      return req;
+      token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -24,7 +24,7 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.error(err);
+      
       console.log('Invalid token');
     }
 

63ba985e737ebeb45237ecb36730152c7528264b
cleclair71 <cassieleclair71@gmail.com>
2023-05-18 17:01:22 -0400

    fixing app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: localStorage.getItem('id_token') ? `Bearer ${localStorage.getItem('id_token')}` : "",
+  },
+});
+
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
+        <Routes>
+
+          <Route path='/' element={<SearchBooks />} />
+
+          <Route path='/saved' element={<SavedBooks />} />
+
+          <Route path='*'
+              element={<h1 className='display-2'>Wrong page!</h1>} />
+
+        </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
